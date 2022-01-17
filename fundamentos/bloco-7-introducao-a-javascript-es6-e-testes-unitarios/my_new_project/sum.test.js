ee9d74c39bcdf8b706db8011d0e375a0d6c4b995
Pérsio Pereira <persiopeo@gmail.com>
2022-01-17 16:02:32 -0300

    Testando a função sum

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/my_new_project/sum.test.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => a + b; // Aqui está a função.
+
+test('sums two values', () => { // Começando o teste.
+  expect(sum(2, 3)).toEqual(5);
+});
+it('somando dois numeros negativos', () => {
+  expect(sum(-2, -6)).toEqual(-8);  
+});
+it('somando numero negativo com positivo', () => {
+  expect(sum(2, -3)).toEqual(-1);  
+});
