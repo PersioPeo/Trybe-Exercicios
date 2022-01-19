e10187d449d239eecd93476075e87d2dfee1478d
jose-cleiton <cleitons835@gmail.com>
2022-01-18 21:40:12 -0300

    Refazendo funçao sum 21:40

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/exercicio-do-curso/sum.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/exercicio-do-curso/sum.js
@@ -1,14 +1,13 @@
-const sum =  (a, b) => {
-    if(typeof a !== 'number' || typeof b !== 'number') {
-        throw new Error('Só pode ser numero');
-    } 
-    return a + b
-
-
-} 
-
-module.exports = sum; // exportando a função sum para outro aquivo para test.
-
+// sum.js
+const sum =(a, b) =>  {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      throw new Error('parameters must be numbers');
+    }
+  
+    return a + b;
+  }
+  
+  module.exports = sum;
 
 
  //estive aqui dnovo no
