04a3cdb1330e5b89467ccfe7a9b1b567d45c7531
Pérsio Pereira <persiopeo@gmail.com>
2022-02-03 16:05:13 -0300

    Adicionando o exercicio-bonus

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus-8.1.js
@@ -0,0 +1,24 @@
+const mage = {
+    healthPoints: 130,
+    intelligence: 45,
+    mana: 125,
+    damage: undefined,
+  };
+  
+  const warrior = {
+    healthPoints: 200,
+    strength: 30,
+    weaponDmg: 2,
+    damage: undefined,
+  };
+  
+  const dragon = {
+    healthPoints: 350,
+    strength: 50,
+    damage: undefined,
+  };
+  
+  const battleMembers = { mage, warrior, dragon };
+/**
+ 1 - Crie a primeira HOF que compõe o objeto gameActions . Ela será a função que simula o turno do personagem warrior . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem warrior e atualizará os healthPoints do monstro dragon . Além disto ela também deve atualizar o valor da chave damage do warrior . 
+ *  */  
