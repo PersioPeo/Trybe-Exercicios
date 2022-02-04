db5474d14e0a9ff71dbaee016bd830bb2854badc
Pérsio Pereira <persiopeo@gmail.com>
2022-02-03 23:42:12 -0300

    Adicionado Parte - 1 do exercicio-bonus

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus-8.1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus-8.1.js
@@ -22,3 +22,51 @@ const mage = {
 /**
  1 - Crie a primeira HOF que compõe o objeto gameActions . Ela será a função que simula o turno do personagem warrior . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem warrior e atualizará os healthPoints do monstro dragon . Além disto ela também deve atualizar o valor da chave damage do warrior . 
  */  
+
+
+/** 1 - Crie uma função que retorna o dano do dragão.
+
+    O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
+ */
+
+const danoDragon = (ojbDragon) => {
+  const danoMinimo = 15;
+  const danoMaximo = Math.floor(Math.random() * (ojbDragon.strength - danoMinimo) + danoMinimo);
+  return danoMaximo;
+}    
+/*
+ 2 - Crie uma função que retorna o dano causado pelo warrior .
+
+    O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
+
+*/
+
+const danoWarrior = (objWarrior) => {
+  const danoMinimo = warrior.strength;
+  const danoMaximo = warrior.weaponDmg * danoMinimo;
+  const danoWarrior = Math.floor(Math.random()* (danoMaximo - danoMinimo +1) + danoMinimo);
+  return danoWarrior;
+}
+
+
+/**
+  3 - Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+
+    O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+    A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
+
+ */
+
+    const atackDoMago = (objMago) => {
+      const magoMana = mage.mana;
+      const danoMinimo = mage.intelligence;
+      const danoMaximo = danoMinimo * 2;
+      const danoMago = Math.floor(Math.random()* (danoMaximo - danoMinimo +1) + danoMinimo);
+
+      if(magoMana < 15) {
+        return "Não possui mana suficiente";
+      } else {
+        return danoMago;
+      }
+    }
+console.log(atackDoMago(mage));
