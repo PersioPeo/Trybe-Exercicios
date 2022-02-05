a88e253d60fe85af258636d67c71c8c20a57e990
Pérsio Pereira <persiopeo@gmail.com>
2022-02-04 22:12:18 -0300

    exerciocio para fixar - 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/hoffind.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/hoffind.js
@@ -34,4 +34,26 @@ let numeroDivPor5;
      let primeiroCom5 = names.find((itemAtual) => itemAtual.length === 5); 
      console.log(primeiroCom5);
     }
-    findNameWithFiveLetters();
+    // findNameWithFiveLetters();
+
+    
+/*
+3 - Utilize o find para encontrar a música com id igual a 31031685 , caso ela exista: 
+*/
+
+    const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(id) {
+  // Adicione seu código aqui
+  let musica = musicas.find((itemAtual) => itemAtual.id === id)
+  return musica;
+}
+
+console.log(findMusic('31031685'))
+    
+
+
