d66b2ec4ea172fdf08040e49d3887e3f7af84640
persiopeo <persiopeo@hotmail.com>
2022-02-18 11:44:14 -0300

    criando o componts Greenting passando uma props

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/react/app/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/react/app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Ola from '../src/componets/Ola';
+import Greeting from './componets/Greeting';
 
 function App() {
   return (
+  <>  
    <Ola />
+   <Greeting name = "Pérsio" /> 
+  </> 
   );
 }
 
