d61c17d7be358c2c2d96ce6229245220f305e7d1
persiopeo <persiopeo@hotmail.com>
2022-02-18 11:29:43 -0300

    Conseguindo criar componete

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/react/app/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/react/app/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Component from './Component'
+import Ola from '../src/componets/Ola';
 
 function App() {
   return (
-    <Component />
+   <Ola />
   );
 }
 
