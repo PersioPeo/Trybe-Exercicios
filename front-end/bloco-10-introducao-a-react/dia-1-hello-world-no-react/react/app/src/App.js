b19f9cc9b74dcd4d6136be37921126eab28520b0
persiopeo <persiopeo@hotmail.com>
2022-02-18 12:19:26 -0300

    Criando um component que mostra a imagem

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/react/app/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/react/app/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Ola from '../src/componets/Ola';
 import Greeting from './componets/Greeting';
+import Imagem from './componets/Imagem';
 
 function App() {
   return (
   <>  
    <Ola />
-   <Greeting name = "Pérsio" /> 
+   <Greeting name = "Pérsio" lastName = "Pereira" /> 
+   <Imagem source = "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText = "Imagem de um gatinho cinza "/>
+
   </> 
   );
 }
