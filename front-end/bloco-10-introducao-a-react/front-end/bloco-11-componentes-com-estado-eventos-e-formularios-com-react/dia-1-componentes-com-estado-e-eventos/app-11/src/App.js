650a97609024d1b9ce4822942ea1ba313b34ca18
persiopeo <persiopeo@hotmail.com>
2022-02-21 15:12:09 -0300

    Exercicio Para Fixar

--- front-end/bloco-10-introducao-a-react/front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/app-11/src/App.js
+++ front-end/bloco-10-introducao-a-react/front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/app-11/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Exercicio01 from './exe-11.js/Exercicio01';
+import ParaFixar from './exe-11.js/ParaFixar';
 
-function App() {
+function App() {  
   return (
     <div className="App">      
       <Exercicio01 />
+      <ParaFixar />
+      <ParaFixar />
     </div>
   );
 }
