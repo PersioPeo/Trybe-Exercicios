9f83e2eeaa8c22dd2471294ea8e99ae6d1b8a182
persiopeo <persiopeo@hotmail.com>
2022-02-21 21:43:40 -0300

    Acesso ao objeto this na função

--- front-end/bloco-10-introducao-a-react/front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/app-11/src/exe-11.js/This.js
+++ front-end/bloco-10-introducao-a-react/front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/app-11/src/exe-11.js/This.js
@@ -1,8 +1,16 @@
 import React from 'react';
-
+//Garanta acesso ao objeto this na função que você declarou.
 class This extends React.Component {
+  constructor() {    
+    super();
+    this.soma = this.soma.bind(this);
+  }
+  soma(a, b){
+    return a + b;
+  }
+
   render() {
-    console.log(this)
+    console.log(this.soma(2, 5));
     return <span>Hello, world!</span>
   }
 }
