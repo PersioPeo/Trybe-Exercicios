c67826204f472485ecc4412df28081e3955c70a9
persiopeo <persiopeo@hotmail.com>
2022-02-23 17:13:45 -0300

    Passando a função para dentro da Class

--- front-end/front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/app01/src/exer01/Tarefas.js
+++ front-end/front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/app01/src/exer01/Tarefas.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React from "react";    
+  
 
-const Task = (value) => {
-    return (
-      <li>{value}</li>
-    );
-  }
-
-  const tarefas = ['Acorda', 'Escovar o dente', 'Tomar café', 'Estudar', 'Almoçar', 'Dormir'];
-
-  class Tarefas extends React.Component{
+ class Tarefas extends React.Component{
+     constructor() {
+         super()
+         this.Task = this.Task.bind(this) 
+     }
+    
+    Task  (value) {
+     return <li>{value}</li>
+    }
       render() {
+
+        const tarefas = ['Acorda', 'Escovar o dente', 'Tomar café', 'Estudar', 'Almoçar', 'Dormir'];
           return(
-            <ul>{tarefas.map((tarefa) => Task(tarefa))}</ul>
+            <ul>{tarefas.map((tarefa) => this.Task(tarefa))}</ul>
           )
       }
   }
