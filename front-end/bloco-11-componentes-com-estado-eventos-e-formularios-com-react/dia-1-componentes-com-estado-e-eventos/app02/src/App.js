829bdc2dc52e46d89e0fcc20c051c9766e9f3d2c
persiopeo <persiopeo@hotmail.com>
2022-02-24 12:08:17 -0300

    incrementar a contagem de cliques cada vez que um botão for clicado

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/app02/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/app02/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import EstEvent3 from './exercicio02/EstEvent3';
 import EstEvent from './exercicio02/EstEvent';
-
 function App() {
   return (
     <div className="App">
+      <EstEvent3 />
       <EstEvent />
     </div>
   );
