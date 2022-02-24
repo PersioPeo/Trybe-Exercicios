3d86a4d4b5ebb1994478fae7d6af6b3054b66d3c
persiopeo <persiopeo@hotmail.com>
2022-02-23 21:28:40 -0300

    Colocando evento no Botao

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/app02/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/app02/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import BtnClick from './exercicio02/BtnClick';
 
 function App() {
   return (
     <div className="App">
-      Olá
+      <BtnClick />
     </div>
   );
 }
---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/app02/src/exercicio02/BtnClick.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+/* Embora isso funcione, essa DEFINITIVAMENTE
+não é a maneira correta de se criar eventos
+em React! A função se refere ao componente,
+então deve ser parte de sua classe! */
+function handleClick() {
+  alert('Clicou no botão!')
+}
+
+class BtnClick extends React.Component {
+  /* Repare que, diferentemente do HTML, no
+  JSX você associa uma função a um evento
+  passando a própria função entre chaves `{}` */
+  render() {
+    return <button onClick={handleClick}>Meu botão</button>
+  }
+}
+
+export default BtnClick;
